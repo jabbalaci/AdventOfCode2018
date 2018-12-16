5b06e20ea2bb82ca8dfbe0e778f8745d2b3abfd0
Jabba Laci <jabba.laci@gmail.com>
2018-12-16 11:07:43 +0100

    sandbox added

--- day16/sandbox/alap2.nim
+++ day16/sandbox/alap2.nim
@@ -1,16 +1,11 @@
-# Thanks to @zacharycarter on IRC for showing me how to store
-# functions in an array.
-
-type
-  fptr = (func(x: int): int)
+# shorter version
 
 func twice(n: int): int =
   2 * n
 
 proc main() =
   let
-    myf: fptr = twice
-    foo: array[1, fptr] = [myf]
+    foo = [twice]
 
   echo twice(4)
   echo foo[0](4)
